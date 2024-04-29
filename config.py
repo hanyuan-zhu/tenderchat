0e6135020bef21a44f5e3b080923313f3c8f7034
buildsense <contact@buildsense.ai>
2024-04-29 17:36:47 +0800

    key

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-api_key = ""
+api_key_qwen = "sk-cbcc1fb859b1456885a270eecbec6369"
 app_secret_key = 'your-secret-key'
 database_config = {
     "host": "gz-cdb-5scrcjb5.sql.tencentcdb.com",
